fb69c60b30870de28bf2e39f0d6b110dcca43fb0
christopher562242-gif <christopher562242@gmail.com>
2026-09-04 17:47:36 +0100

    Fix environment configuration

--- src/config/env.js
+++ src/config/env.js
@@ -1,15 +1,1 @@
-const dotenv = require('dotenv');
-
-dotenv.config();
-
-const PORT = process.env.PORT || 5000;
-const GNEWS_API_KEY = process.env.GNEWS_API_KEY;
-
-if (!GNEWS_API_KEY) {
-  throw new Error('GNEWS_API_KEY is missing from .env');
-}
-
-module.exports = {
-  PORT,
-  GNEWS_API_KEY,
-};
+Get-ChildItem -Recurse -File | Select-String -Pattern "GNEWS_API_KEY|GNEWS"
